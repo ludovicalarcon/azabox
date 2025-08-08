09a317fc91abe82adef81e0ad19719a52b65ea57
Ludovic Alarcon <alarcon.ludovic@gmail.com>
2025-08-08 19:12:32 +0900

    refacto(set): improve set

--- internal/types/set.go
+++ internal/types/set.go
@@ -1,5 +1,10 @@
 package types
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Set[T comparable] map[T]struct{}
 
 func NewSet[T comparable](size ...int) Set[T] {
@@ -9,13 +14,39 @@ func NewSet[T comparable](size ...int) Set[T] {
 	return make(Set[T])
 }
 
-func (s Set[T]) Add(item T) {
-	s[item] = struct{}{}
+func SetFromSlice[T comparable](items []T) Set[T] {
+	set := NewSet[T](len(items))
+	for _, item := range items {
+		set[item] = struct{}{}
+	}
+	return set
+}
+
+func (s Set[T]) Add(items ...T) {
+	for _, item := range items {
+		s[item] = struct{}{}
+	}
+}
+
+func (s Set[T]) Remove(items ...T) {
+	for _, item := range items {
+		delete(s, item)
+	}
 }
 
 func (s Set[T]) Has(item T) bool {
 	_, ok := s[item]
 	return ok
+}
+
+func (s Set[T]) Len() int { return len(s) }
+
+func (s Set[T]) Empty() bool { return s.Len() == 0 }
+
+func (s Set[T]) Clear() {
+	for item := range s {
+		delete(s, item)
+	}
 }
 
 func (s Set[T]) ToSlice() []T {
@@ -24,4 +55,23 @@ func (s Set[T]) ToSlice() []T {
 		slice = append(slice, key)
 	}
 	return slice
+}
+
+func (s Set[T]) String() string {
+	if s == nil || s.Empty() {
+		return "{}"
+	}
+
+	var sb strings.Builder
+	i, length := 0, s.Len()
+	sb.WriteString("{")
+	for item := range s {
+		sb.WriteString(fmt.Sprintf("%v", item))
+		if i < length-1 {
+			sb.WriteString(" ")
+		}
+		i++
+	}
+	sb.WriteString("}")
+	return sb.String()
 }
--- internal/types/set_test.go
+++ internal/types/set_test.go
@@ -17,6 +17,27 @@ func TestNewSet(t *testing.T) {
 	})
 }
 
+func TestSetFromSlice(t *testing.T) {
+	t.Run("should create a new set from a slice", func(t *testing.T) {
+		sliceString := []string{"foo", "bar"}
+		sliceInt := []int{10, 42, 7, 19}
+
+		setS := SetFromSlice(sliceString)
+		setI := SetFromSlice(sliceInt)
+
+		require.NotNil(t, setS)
+		require.NotNil(t, setI)
+		assert.Len(t, setS, len(sliceString))
+		assert.Len(t, setI, len(sliceInt))
+		for key := range setS {
+			assert.Contains(t, sliceString, key, fmt.Sprintf("item %s should be in Set ", key))
+		}
+		for key := range setI {
+			assert.Contains(t, sliceInt, key, fmt.Sprintf("item %d should be in Set ", key))
+		}
+	})
+}
+
 func TestSet_Add(t *testing.T) {
 	t.Run("should add item", func(t *testing.T) {
 		set := NewSet[int]()
@@ -33,9 +54,7 @@ func TestSet_Add(t *testing.T) {
 		set := NewSet[string]()
 		expected := []string{"toto", "tata", "titi", "tutu"}
 
-		for _, item := range expected {
-			set.Add(item)
-		}
+		set.Add(expected[0], expected[1], expected[2], expected[3])
 
 		assert.Len(t, set, len(expected))
 		for key := range set {
@@ -56,6 +75,97 @@ func TestSet_Add(t *testing.T) {
 		for key := range set {
 			assert.Contains(t, expected, key, fmt.Sprintf("item %s should be in Set ", key))
 		}
+	})
+}
+
+func TestSet_Remove(t *testing.T) {
+	t.Run("should remove single item", func(t *testing.T) {
+		set := NewSet[int](4)
+		set.Add(42, 10, 1, 19)
+
+		set.Remove(10)
+		assert.Len(t, set, 3)
+		assert.False(t, set.Has(10))
+	})
+
+	t.Run("should remove single item", func(t *testing.T) {
+		set := NewSet[int](4)
+		set.Add(42, 10, 1, 19)
+
+		set.Remove(10, 19)
+		assert.Len(t, set, 2)
+		assert.False(t, set.Has(10))
+		assert.False(t, set.Has(19))
+	})
+
+	t.Run("should handle empty", func(t *testing.T) {
+		set := NewSet[int]()
+		set.Remove(10)
+		assert.Len(t, set, 0)
+	})
+}
+
+func TestSet_Len(t *testing.T) {
+	t.Run("should return set len", func(t *testing.T) {
+		testCases := []struct {
+			name string
+			set  Set[int]
+
+			expected int
+		}{
+			{
+				name:     "empty",
+				set:      NewSet[int](),
+				expected: 0,
+			},
+			{
+				name:     "single item",
+				set:      SetFromSlice([]int{42}),
+				expected: 1,
+			},
+			{
+				name:     "multiple item",
+				set:      SetFromSlice([]int{42, 1, 19, 7, 14}),
+				expected: 5,
+			},
+		}
+		for _, tc := range testCases {
+			t.Run(tc.name, func(t *testing.T) {
+				assert.Equal(t, tc.expected, tc.set.Len())
+			})
+		}
+	})
+}
+
+func TestSet_Empty(t *testing.T) {
+	t.Run("should return true on empty set", func(t *testing.T) {
+		set := NewSet[int]()
+		assert.True(t, set.Empty())
+	})
+
+	t.Run("should return false on non empty set", func(t *testing.T) {
+		set := NewSet[int]()
+		set.Add(42, 10)
+		assert.False(t, set.Empty())
+	})
+}
+
+func TestSet_Clear(t *testing.T) {
+	t.Run("should empty a set", func(t *testing.T) {
+		set := NewSet[string](3)
+		set.Add("foo", "bar", "toto")
+
+		set.Clear()
+
+		assert.Empty(t, set)
+	})
+
+	t.Run("should handle empty set", func(t *testing.T) {
+		set := NewSet[string]()
+
+		set.Clear()
+
+		assert.Empty(t, set)
 	})
 }
 
@@ -142,6 +252,40 @@ func TestSet_ToSlice(t *testing.T) {
 
 				require.Len(t, slice, len(tc.expected))
 				assert.Equal(t, tc.expected, slice)
+			})
+		}
+	})
+}
+
+func TestSet_String(t *testing.T) {
+	t.Run("should stringify set", func(t *testing.T) {
+		testCases := []struct {
+			name     string
+			set      Set[string]
+			expected string
+		}{
+			{
+				name:     "empty",
+				set:      NewSet[string](),
+				expected: "{}",
+			},
+			{
+				name:     "nil",
+				set:      nil,
+				expected: "{}",
+			},
+			// using single values as underling datastructure is a map
+			// and in go map doesn't have a deterministic order
+			{
+				name:     "values",
+				set:      SetFromSlice([]string{"foo"}),
+				expected: "{foo bar toto}",
+			},
+		}
+
+		for _, tc := range testCases {
+			t.Run(tc.name, func(t *testing.T) {
+				assert.Equal(t, tc.expected, tc.set.String())
 			})
 		}
 	})
